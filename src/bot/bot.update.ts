f5360110a091a6938b893b4ba2d26a129a15dc2e
ker4ik13 <kekustop228@mail.ru>
2025-12-21 20:25:19 +0300

    fix: исправлена логика выдачи промпта

--- src/bot/bot.update.ts
+++ src/bot/bot.update.ts
@@ -5,6 +5,7 @@ import { CHANNELS_LINKS } from 'lib/common';
 import { getNormalChatId, getValueFromAction } from 'lib/helpers';
 import { emojis } from 'lib/utils';
 import { Action, Ctx, InjectBot, Start, Update } from 'nestjs-telegraf';
+import { join } from 'path';
 import { Context, Input, Telegraf } from 'telegraf';
 import { SceneContext } from 'telegraf/scenes';
 import { BotService } from './bot.service';
@@ -14,6 +15,12 @@ import {
   promptKeyboard,
 } from './keyboards';
 import { mainMessages } from './messages';
+
+const myPromptFiles = {
+  '50': join(__dirname, '..', '..', 'files', `prompts50.pdf`),
+  '100': join(__dirname, '..', '..', 'files', `prompts100.pdf`),
+  '107': join(__dirname, '..', '..', 'files', `prompts107.pdf`),
+};
 
 @Update()
 export class BotUpdate {
@@ -64,9 +71,11 @@ export class BotUpdate {
   async downloadFile50(@Ctx() ctx: Context) {
     const promptFileName = getValueFromAction(ctx, 2, '-');
     const file = Input.fromLocalFile(
-      this.botService.getPromptFilePath(promptFileName),
+      myPromptFiles[promptFileName],
       `${promptFileName} промптов для фото Нейролюб.pdf`,
     );
+
+    const loadingMessage = await ctx.reply(`${emojis.refresh} Загрузка...`);
 
     await ctx.replyWithDocument(file, {
       caption: mainMessages.successMessage,
@@ -74,6 +83,8 @@ export class BotUpdate {
         inline_keyboard: goToHomeKeyboard(),
       },
     });
+
+    await ctx.deleteMessage(loadingMessage.message_id);
 
     // Сообщение для ведения статистики в админ канал
     const downloadMessage = `${emojis.checkmark} Скачан файл – ${promptFileName} промптов\n${emojis.user} ${ctx.from.first_name} (@${ctx.from.username})\n${emojis.calendar} ${new Date().toLocaleString('ru')}`;
